4af1b557270835df304e7b596ee0dc2b86ab8354
Oswin So <oswinso@gmail.com>
2024-03-18 13:00:33 -0400

    BuRd trunc

--- src/og/cmap.py
+++ src/og/cmap.py
@@ -16,3 +16,17 @@ def get_BuRd():
 
     sdf_cm = LinearSegmentedColormap.from_list("SDF", [(0, light_blue), (0.5, blue), (0.5, red), (1, light_red)], N=256)
     return sdf_cm
+
+
+def get_BuRd_trunc(trunc_frac: float):
+    """Get only the middle part of the BuRd colormap. trunc_frac should be in [0, 1]."""
+    sdf_cm = get_BuRd()
+
+    light_blue = sdf_cm(0.5 * trunc_frac)
+    light_red = sdf_cm(1.0 - 0.5 * trunc_frac)
+
+    blue = hsl2hex([0.57, 0.5, 0.55])
+    red = hsl2hex([0.028, 0.62, 0.59])
+
+    sdf_cm = LinearSegmentedColormap.from_list("SDF", [(0, light_blue), (0.5, blue), (0.5, red), (1, light_red)], N=256)
+    return sdf_cm
