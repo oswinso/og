619c1f05080100e817e07ac88e91902320e5ca70
Oswin So <oswinso@gmail.com>
2024-09-09 13:39:54 -0400

    Add remap box inv

--- src/og/remap.py
+++ src/og/remap.py
@@ -1,3 +1,8 @@
 def remap_box(x, lo, hi):
     """Remap x from [-1, 1] to [lo, hi]."""
     return 0.5 * (x + 1) * (hi - lo) + lo
+
+
+def remap_box_inv(y, lo, hi):
+    """The inverse of remap_box. From [lo, hi] to [-1, 1]"""
+    return 2 * (y - lo) / (hi - lo) - 1
