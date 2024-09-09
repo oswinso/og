9a24682629a8b1ff1d37dc603e285e146dd5e446
Oswin So <oswinso@gmail.com>
2024-09-09 13:47:01 -0400

    plot one-sided bounds

--- src/og/plot_phase_utils.py
+++ src/og/plot_phase_utils.py
@@ -21,18 +21,21 @@ def plot_x_bounds(ax: plt.Axes, bounds: tuple[float | None, float | None], obs_s
         ax.add_patch(rect)
 
 
-def plot_y_bounds(ax: plt.Axes, bounds: tuple[float, float], obs_style: dict):
+def plot_y_bounds(ax: plt.Axes, bounds: tuple[float | None, float | None], obs_style: dict):
     xmin, xmax = ax.get_xlim()
     ymin, ymax = ax.get_ylim()
     xlen, ylen = xmax - xmin, ymax - ymin
     lb, ub = bounds
 
     # Lower bounds.
-    rect = plt.Rectangle((xmin, ymin), xlen, lb - ymin, **obs_style)
-    ax.add_patch(rect)
+    if lb is not None:
+        rect = plt.Rectangle((xmin, ymin), xlen, lb - ymin, **obs_style)
+        ax.add_patch(rect)
+
     # Upper bounds.
-    rect = plt.Rectangle((xmin, ub), xlen, ymax - ub, **obs_style)
-    ax.add_patch(rect)
+    if ub is not None:
+        rect = plt.Rectangle((xmin, ub), xlen, ymax - ub, **obs_style)
+        ax.add_patch(rect)
 
 
 def plot_x_goal(ax: plt.Axes, bounds: tuple[float, float], goal_style: dict):
