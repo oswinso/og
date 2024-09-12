9828a4c4ac1342303064f2bf473b4b4476560df4
Oswin So <oswinso@gmail.com>
2024-09-12 11:25:38 -0400

    ez_axes

---
+++ src/og/plot/ez_axes.py
@@ -0,0 +1,34 @@
+from typing import overload
+
+import matplotlib.pyplot as plt
+
+
+class EzAxes:
+    def __init__(self, axes: list[plt.Axes]):
+        self.axes = axes
+        self.idx = 0
+
+    @property
+    def __len__(self):
+        return len(self.axes)
+
+    def get(self, n_axes: int | None = None):
+        if n_axes is None:
+            ax = self.axes[self.idx]
+            self.idx += 1
+            if self.idx >= len(self.axes):
+                raise IndexError("No more axes available.")
+
+            return ax
+
+        axes = self.axes[self.idx : self.idx + n_axes]
+        self.idx += n_axes
+        if self.idx >= len(self.axes):
+            raise IndexError("No more axes available.")
+        return axes
+
+    @overload
+    def get(self) -> plt.Axes: ...
+
+    @overload
+    def get(self, n_axes: int) -> list[plt.Axes]: ...
