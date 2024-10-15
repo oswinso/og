a781b2b22e48dd641de31b611f79f30f13bf04dd
Oswin So <oswinso@gmail.com>
2024-10-15 19:09:46 -0400

    change from dot to sum

--- src/og/sdf/tri.py
+++ src/og/sdf/tri.py
@@ -1,5 +1,5 @@
-import numpy as np
 import jumpy.numpy as jp
+import numpy as np
 
 
 def in_triangle(pos: np.ndarray, p0: np.ndarray, p1: np.ndarray, p2: np.ndarray):
@@ -8,11 +8,11 @@ def in_triangle(pos: np.ndarray, p0: np.ndarray, p1: np.ndarray, p2: np.ndarray)
     v1 = p1 - p0
     v2 = pos - p0
 
-    dot00 = jp.dot(v0, v0)
-    dot01 = jp.dot(v0, v1)
-    dot02 = jp.dot(v0, v2)
-    dot11 = jp.dot(v1, v1)
-    dot12 = jp.dot(v1, v2)
+    dot00 = jp.sum(v0 * v0, axis=-1)
+    dot01 = jp.sum(v0 * v1, axis=-1)
+    dot02 = jp.sum(v0 * v2, axis=-1)
+    dot11 = jp.sum(v1 * v1, axis=-1)
+    dot12 = jp.sum(v1 * v2, axis=-1)
 
     inv_denom = 1 / (dot00 * dot11 - dot01 * dot01)
     u = (dot11 * dot02 - dot01 * dot12) * inv_denom
